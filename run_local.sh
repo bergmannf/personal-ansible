4745f3086f4eb7fedb83cc2564c94fba85b33711
Florian Bergmann <Bergmann.F@gmail.com>
2020-03-09 15:39:11 +0100

    No dependencies currently used

--- run_local.sh
+++ run_local.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-echo "Installing dependencies"
-ansible-galaxy install --roles-path=roles -r requirements.yml
-
 echo "Running playbook"
 ansible-playbook --inventory="localhost," --connection=local --become --ask-become-pass all.yml
