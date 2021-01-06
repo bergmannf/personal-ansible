18a67a6e8f19d9d71e7aaf2ad14f9c7b90382797
Florian Bergmann <Bergmann.F@gmail.com>
2021-01-06 11:44:08 +0100

    Use gpg-agent for ssh-keys as well.

--- roles/zsh/templates/ssh-agent.zsh
+++ roles/zsh/templates/ssh-agent.zsh
@@ -1,31 +1,8 @@
-typeset ssh_environment
-
-function start_ssh_agent() {
-	  local lifetime
-	  local -a identities
-
-	  zstyle -s :plugins:ssh-agent lifetime lifetime
-
-	  ssh-agent -s ${lifetime:+-t} ${lifetime} | sed 's/^echo/#echo/' >! $ssh_environment
-	  chmod 600 $ssh_environment
-	  source $ssh_environment > /dev/null
-
-	  zstyle -a :plugins:ssh-agent identities identities
-
-	  echo starting ssh-agent...
-	  ssh-add $HOME/.ssh/${^identities}
-}
-
-ssh_environment="$HOME/.ssh/environment-$HOST"
-
-if [[ -f "$ssh_environment" ]]; then
-	  source $ssh_environment > /dev/null
-	  ps x | grep ssh-agent | grep -q $SSH_AGENT_PID || {
-		    start_ssh_agent
-	  }
-else
-	  start_ssh_agent
+# See: https://wiki.archlinux.org/index.php/GnuPG#Configuration_2
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 fi
 
-unset ssh_environment
-unfunction start_ssh_agent
+export GPG_TTY=$(tty)
+gpg-connect-agent updatestartuptty /bye >/dev/null
