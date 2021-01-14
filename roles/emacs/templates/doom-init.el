17d38a9615de3f8818d88b7ba84692154801e4ff
Florian Bergmann <Bergmann.F@gmail.com>
2021-01-14 11:14:06 +0100

    Use poetry and lsp when using python.

--- roles/emacs/templates/doom-init.el
+++ roles/emacs/templates/doom-init.el
@@ -153,7 +153,9 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python             ; beautiful is better than ugly
+        +poetry
+        +lsp)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
