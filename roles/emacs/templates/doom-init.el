2bba8164e7c0d5c25875ca2bab8c7146f7427215
Florian Bergmann <bergmann.f@gmail.com>
2023-12-15 09:43:04 +0100

    Use eglot instead of lsp-mode.

--- roles/emacs/templates/doom-init.el
+++ roles/emacs/templates/doom-init.el
@@ -88,7 +88,7 @@
        :tools
        ansible
        ;; biblio
-       (debugger +lsp)     ; FIXME stepping through code, to help you add bugs
+       debugger            ; FIXME stepping through code, to help you add bugs
        direnv
        docker
        editorconfig        ; let someone else argue about tabs vs spaces
@@ -96,7 +96,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       lsp
+       (lsp +eglot)
        magit               ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
